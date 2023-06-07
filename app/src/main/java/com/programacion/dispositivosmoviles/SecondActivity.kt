69987efb4a2f67107284a81251105552f30d31e5
Paul Merizalde <merizaldepaul1999>
2023-06-06 19:38:27 -0500

    navigation bar

--- app/src/main/java/com/programacion/dispositivosmoviles/SecondActivity.kt
+++ app/src/main/java/com/programacion/dispositivosmoviles/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.programacion.dispositivosmoviles.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
@@ -19,13 +20,45 @@ class SecondActivity : AppCompatActivity() {
         super.onStart()
         Log.d("UCE", "Loading...")
 
-        var name = ""
-        intent.extras.let {
-            name = it?.getString("var1")!!
-        }
-        Log.d("UCE", "Hola ${name}")
+        var name = "USUARIO"
+//        intent.extras.let {
+//            name = it?.getString("var1")!!
+//        }
+//        Log.d("UCE", "Hola ${name}")
         binding.txtWelcome.text = "Bienvenido " + name
         initClass()
+        binding.bottomNavigation.setOnItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.item_1 -> {
+                    var suma = 0
+                    for (i in listOf(8,10,14)){
+                        suma+=i
+                    }
+                    Snackbar.make(
+                        binding.txtWelcome,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setBackgroundTint(getColor(R.color.snackbarColor))
+                        .show()
+                    true
+                }
+                R.id.item_2 -> {
+                    var suma = 0
+                    for (i in listOf(10,16,23)){
+                        suma+=i
+                    }
+                    Snackbar.make(
+                        binding.txtWelcome,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    ).setBackgroundTint(getColor(R.color.snackbarColor))
+                        .show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun initClass() {
@@ -34,7 +67,8 @@ class SecondActivity : AppCompatActivity() {
                 this,
                 MainActivity::class.java
             )
-            startActivity(intent)
+//            startActivity(intent)
         }
     }
+
 }
